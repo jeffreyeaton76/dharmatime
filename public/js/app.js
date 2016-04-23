08075593b6a52b28ad0a550ca5b35005859c60a9
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-23 17:40:33 -0400

    creates template in public/views

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,23 @@
 "use strict";
 (function(){
-  angular.module("timer", ["ngResource"])
+  angular.module("timer", ["ngResource", "ui.router"])
+  .config([
+    "$stateProvider",
+    routerFunction
+  ])
   .factory("sitFactory", sitFactoryFunc)
   .controller("timerController", ["sitFactory", timerControllerFunc])
 
 
-
+function routerFunction($stateProvider){
+  $stateProvider
+  .state("index", {
+    url:"/",
+    templateUrl:"/views/index.html",
+    controller:"timerController",
+    controllerAs:"tc"
+  })
+}
 function timerControllerFunc(Sit){
   this.sits = Sit.query()
 }
