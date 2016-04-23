4c3195380a8bc6d99808b198629ace2b627b4639
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-23 16:36:49 -0400

    writes to database

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 var express = require("express");
+var hbs = require("express-handlebars");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/timer");
+
+var Sit = mongoose.model("Sit", {
+  date: {type: Date},
+  duration_set: Number,
+  duration: Number
+});
+
 var app = express();
 
-app.get("/", function(req, res){
-  res.send("working");
+Sit.create({
+  date: '2015-12-08',
+  duration_set: 20,
+  duration: 20
 });
+
+app.get("/api/sits", function(req, res){
+  Sit.find().then(function(err, sits){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.json(sits);
+    }
+  });
+});
+
+// app.get("/", function(req, res){
+//   res.send("working");
+// });
 
 app.listen(3000);
