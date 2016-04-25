98d31826f41d5fca0a5f64d81882362dce23f6a4
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-04-25 11:07:40 -0400

    reverts to pre-gulp

--- public/js/app.js
+++ public/js/app.js
@@ -7,23 +7,35 @@
   ])
   .factory("sitFactory", sitFactoryFunc)
   .controller("timerController", ["sitFactory", timerControllerFunc])
+  .controller("indexController", ["sitFactory", indexControllerFunc]);
 
 
-function routerFunction($stateProvider){
-  $stateProvider
-  .state("index", {
-    url:"/",
-    templateUrl:"/views/index.html",
-    controller:"timerController",
-    controllerAs:"tc"
-  })
-}
-function timerControllerFunc(Sit){
-  this.sits = Sit.query()
-}
+  function routerFunction($stateProvider){
+    $stateProvider
+    .state("index", {
+      url:"/",
+      templateUrl:"/views/index.html",
+      controller:"timerController",
+      controllerAs:"tc"
+    })
+    .state("sits", {
+      url:"/sits",
+      templateUrl:"/views/sits.html",
+      controller:"timerController",
+      controllerAs:"tc"
+    });
+  }
 
-function sitFactoryFunc($resource){
-  return $resource("/api/sits/:id")
-}
+  function timerControllerFunc(Sit){
+    this.sits = Sit.query();
+  }
+
+  function indexControllerFunc(Sit){
+    this.sits = Sit.query();
+  }
+
+  function sitFactoryFunc($resource){
+    return $resource("/api/sits/:id");
+  }
 
 })();
