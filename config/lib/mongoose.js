b98a809ab0d689e3781c894bcbf8dc2b4a332114
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-06-11 18:31:19 -0400

    adds user model to mongoose

--- config/lib/mongoose.js
+++ config/lib/mongoose.js
@@ -19,4 +19,18 @@ var SitSchema = new mongoose.Schema(
   }
 );
 
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
 module.exports.Sit = mongoose.model("Sit", SitSchema);
---
+++ config/lib/users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var mongoose = require('./mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  hash: String,
+  salt: String
+});
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+  return this.hash === hash;
+};
+
+module.exports.Sit = mongoose.model("User", userSchema);
