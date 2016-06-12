5f43c7da7c53e23951858e110f2d17e97ee5a515
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-06-12 14:10:21 -0400

    adds angular suthenitcation service

---
+++ public/js/resources/authentication-service.js
@@ -0,0 +1,68 @@
+angular.module('timer')
+.service('authentication', authentication);
+
+authentication.$inject = ['$http', '$window'];
+function authentication ($http, $window) {
+
+  var saveToken = function (token) {
+    $window.localStorage['mean-token'] = token;
+  };
+
+  var getToken = function () {
+    return $window.localStorage['mean-token'];
+  };
+
+  logout = function() {
+    $window.localStorage.removeItem('mean-token');
+  };
+
+  var isLoggedIn = function() {
+    var token = getToken();
+    var payload;
+
+    if(token){
+      payload = token.split('.')[1];
+      payload = $window.atob(payload);
+      payload = JSON.parse(payload);
+
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  var currentUser = function() {
+    if(isLoggedIn()){
+      var token = getToken();
+      var payload = token.split('.')[1];
+      payload = $window.atob(payload);
+      payload = JSON.parse(payload);
+      return {
+        email : payload.email,
+        name : payload.name
+      };
+    }
+  };
+
+  register = function(user) {
+    return $http.post('/api/register', user).success(function(data){
+      saveToken(data.token);
+    });
+  };
+
+  login = function(user) {
+    return $http.post('/api/login', user).success(function(data) {
+      saveToken(data.token);
+    });
+  };
+
+  return {
+    saveToken : saveToken,
+    getToken : getToken,
+    logout : logout,
+    isLoggedIn : isLoggedIn,
+    currentUser : currentUser,
+    register : register,
+    login : login
+  };
+}
